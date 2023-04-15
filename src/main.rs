74ae9e66e1a5f624a146fb8f6e1ee7563a7e697d
rvcas <x@rvcas.dev>
2023-04-15 14:43:03 -0400

    feat: monaco is rendering

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use leptos::*;
-use monaco::api::CodeEditor as CodeEditorModel;
+use monaco::{
+    api::{CodeEditor as CodeEditorModel, CodeEditorOptions},
+    sys::editor::BuiltinTheme,
+};
 
 #[component]
 fn CodeEditor(cx: Scope, initial_value: i32) -> impl IntoView {
     let node_ref = create_node_ref(cx);
     let (editor, set_editor) = create_signal(cx, None);
 
-    node_ref.on_load(cx, |element| {
+    node_ref.on_load(cx, move |element| {
         use wasm_bindgen::JsCast;
 
         let div_element: &web_sys::HtmlDivElement = &element;
         let html_element = div_element.unchecked_ref::<web_sys::HtmlElement>();
-        let e = CodeEditorModel::create(html_element, None);
+
+        let options = CodeEditorOptions::default()
+            .with_language("rust".to_string())
+            .with_value("main.rs".to_string())
+            .with_builtin_theme(BuiltinTheme::VsDark)
+            .with_new_dimension(1000, 1000);
+
+        let e = CodeEditorModel::create(html_element, Some(options));
 
         set_editor(Some(e))
     });
