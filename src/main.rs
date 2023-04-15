9ca2bf46e87033baf4b662d3b656333338efb84f
rvcas <x@rvcas.dev>
2023-04-15 14:12:52 -0400

    feat: start trying to use monaco

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use leptos::*;
+use monaco::api::CodeEditor as CodeEditorModel;
+use wasm_bindgen::JsCast;
+
+#[component]
+fn CodeEditor(cx: Scope, initial_value: i32) -> impl IntoView {
+    let node_ref = create_node_ref(cx);
+    let (editor, set_editor) = create_signal(cx, None);
+
+    node_ref.on_load(cx, |element| {
+        let e = CodeEditorModel::create(&element, None);
+
+        set_editor(Some(e))
+    });
+
+    view! { cx,
+        <div _ref=node_ref>
+        </div>
+    }
+}
 
 fn main() {
     mount_to_body(|cx| {
         view! { cx,
             <p>
-                "Hello, world!"
+                <CodeEditor initial_value=0 />
             </p>
         }
     })
